fd69b142f00a22afb06628f11872c496b81f71a8
dmuneto1 <dmuneto1@eafit.edu.com>
2017-10-08 18:25:50 -0500

    python generator matrix

---
+++ generatorPython.py
@@ -0,0 +1,39 @@
+import random
+
+file= open("matrix.txt",'w')
+
+def randomGenerator(size,rango):
+    sum = 0
+    sol=[]
+    for i in range (size):
+            randVal =random.randrange(rango)+1
+            sum+= randVal
+            sol.append(randVal)
+    sol.append(randVal)
+    return sol
+
+def matrixGenerator(size,rango):    
+    for i in range(size):
+        actual = randomGenerator(size,rango)
+        bVal=0
+        line=""
+        for j in range(size):
+            if i==j:
+                printVal=actual[len(actual)-1]
+            else:
+                printVal = actual[j]
+            line+=str(printVal)+" "
+            
+            bVal += (j+1) * printVal
+        line=line + str(bVal)
+        #print line
+        file.write(line+"\n")
+        
+def main():
+    rango=9
+    size=10
+    file.write(str(size)+"\n")
+    matrixGenerator(size,rango)
+
+main()
+ 
