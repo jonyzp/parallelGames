31e0164ebec2f2f7c5ef47c8585e74be7b762bef
Mauricio Hoyos Ardila <mauriciohoyosardila>
2017-10-10 03:14:59 -0500

    gauss listo

--- gauss.cpp
+++ gauss.cpp
@@ -5,71 +5,65 @@
 #include<curses.h>
 #include<stdlib.h>
 #include<fstream>
+
 using namespace std;
 int i,j,k,n,row,column;
-double m[10001][10001],aux;
+const int tam=10000;
+double m[tam][tam],aux, X[tam];
 
 int main(){
-  
-/*
-  std::cout<<"        <<Gaussian Method>>         ";
-  std::cout<<"\n Square matrix of order N= ";
-  std::cin>>n;
-  std::cout<<"\n Type the matrix's elements ";
-  for(i=1;i<=n;i++){
-    for(j=1;j<=n;j++){
-      std::cout<<"\n M=["<<i<<","<<j<<"]= ";
-      std::cin>>m[i][j];
-    }
-    std::cout<<"\n Independent term of X"<<i<<" ";
-    std::cin>>m[i][n+1];
-  }
-*/
-ifstream f("matrix.txt");
-f >> n;
 
 
+time_t start = time(0);
 
-for (i = 1; i <= n; i++){
-  for (j = 1; j <= n; j++){
-      f >> m[i][j];
-  }
-  f >> m[i][n+1];
-}  
-
-//#pragma omp parallel for private( k,i,j,aux)
-for(i=1;i<=n;i++){
-  if(m[i][i]!=0){
-    aux=1/m[i][i];
-    for(j=1;j<=n+1;j++){
-      m[i][j]=aux*m[i][j];
-    }
-
-    for(j=1;j<=n;j++){
-      if(j!=i){
-        aux=-m[j][i];
-        for(k=1;k<=n+1;k++){
-          m[j][k]=m[j][k]+aux*m[i][k];
-        }
+for(k=0; k<n; ++k){
+    for(i=k+1; i<n; ++i){
+      aux = m[i][k]/m[k][k];
+      m[i][k] =0;
+      for(j=k+1; j<=n; ++j){
+        m[i][j] = double(m[i][j] - aux*m[k][j]);
       }
     }
   }
+
+for(int i=n-1; i>=0; --i){
+  double sum = 0;
+  for(int p=i+1; p<n; ++p){
+    sum += m[i][p]*X[p];
+  }
+  X[i]=(m[i][n]-sum)/m[i][i];
 }
 
-std::cout<<"\n";
-std::cout<<"The identity matrix is";
-std::cout<<"\n\n";
+time_t end = time(0);
+double tim = difftime(end, start) * 1000.0;
+
+
+/*
+cout<<"The identity matrix is";
+cout<<"\n\n";
+for(i=0;i<n;i++){
+  for(j=0;j<n+1;j++){
+    cout<<m[i][j]<<"\t";
+  }
+  cout<<"\n\n";
+}
+
+
+cout<<"\n";
+
+cout<<"The identity matrix is";
+cout<<"\n\n";
 for(i=1;i<=n;i++){
   for(j=1;j<=n;j++){
-    std::cout<<m[i][j]<<"\t";
+    cout<<m[i][j]<<"\t";
   }
-  std::cout<<"\n\n";
+  cout<<"\n\n";
 }
-
-std::cout<<"The value of unknowns is : ";
+*/
+cout<<"The value of unknowns is : ";
 for(i=1;i<=n;i++){
-  std::cout<<"\nX"<<i<<" = "<<m[i][n+1]<<"\n";
+  cout<<"\nX "<<i<<" = "<<X[i]<<"\n";
 }
 
-getchar();
+cout << tim;
 }
