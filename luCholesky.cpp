60ba836d96da374c712079ecd32fdb284b5ccc6d
Mateo <hincapiemateo@gmail.com>
2017-10-17 09:02:21 -0500

    Lu cholesky melo

--- luCholesky.cpp
+++ luCholesky.cpp
@@ -7,9 +7,13 @@ double **L, **U, *B, *Z, *X, **A;
 void readMatrix( int size,string origin){
 	B = new double[size];
     U = new double *[size];
-    
+    A= new double *[size];
     for (int i = 0; i < size; ++i){
         U[i] = new double [size];
+    }
+
+    for (int i = 0; i < size; ++i){
+        A[i] = new double [size];
     }
 
     L = new double*[size];
@@ -23,7 +27,7 @@ void readMatrix( int size,string origin){
     read >> B[0];
     for (int i = 0; i < size; ++i){
         for (int j = 0; j < size; ++j){
-            read >> U[i][j];
+            read >> A[i][j];
         }
         read >> B[i];
     }
@@ -45,6 +49,34 @@ void gaussianElimination(int n){
 	}
 }
 
+void cholesky(int n){
+    long long suma1,suma2,suma3;
+    
+    for(int k=0;k<n;++k){
+        suma1=0;
+        for(int m=0;m<k-1;++m){
+            suma1+=L[k][m]*U[m][k];
+        }
+        L[k][k]=sqrt(A[k][k]-suma1);
+        U[k][k]=L[k][k];
+        for(int i=k+1;i<n;++i){
+            suma2=0;
+            for(int p=0;p<k-1;++p){
+                suma2+=L[i][p]*U[p][k];
+            }
+            L[i][k]=(A[i][k]-suma2)/L[k][k];
+        }
+        
+        for(int j=k+1;j<n;++j){
+            suma3=0;
+            for(int h=0;h<k-1;++h){
+                suma3+=L[k][h]*U[h][j];
+            }
+            U[k][j]=(A[k][j]-suma3)/L[k][k];
+         
+        }
+    }
+}
 
 void progressiveC(int n){
 	Z = new double[n];
@@ -74,12 +106,8 @@ void printSolution(string solutionFile, int size){
     for (int i = 0; i <size; ++i){
         write << X[i] << endl;
     }
-    for (int i = 0; i <size; ++i){
-        for (int j = 0; j <size; ++j){
-            cout << A[i][j] <<" " ;
-        }
-        cout << endl;
-    }
+    
+
     write.close();
 }
 
@@ -90,7 +118,7 @@ int main(){
     solutionFile="solution.txt";
     ifstream f("matrix.txt");
     f >> matrixSize; 
-    A = new double*[matrixSize];    
+    /*A = new double*[matrixSize];    
     for (int i = 0; i < matrixSize; ++i){
 		A[i] = new double[matrixSize];
     }
@@ -99,9 +127,10 @@ int main(){
             f >> A[i][j];
         }       
     } 
+*/
+
     readMatrix(matrixSize,originalFile);
-    gaussianElimination(matrixSize);
-    //solveCrout(matrixSize);
+    cholesky(matrixSize);
     progressiveC(matrixSize);
     regressiveC(matrixSize);
     printSolution(solutionFile, matrixSize);
