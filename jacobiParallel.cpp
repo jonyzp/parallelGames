5679ecc92ccc6c2a2e9ab24a1b53abf0f2b3723e
Mauricio Hoyos Ardila <mauriciohoyosardila>
2017-11-09 13:31:15 -0500

    ya esta listo cholesky en parallelo

--- jacobiParallel.cpp
+++ jacobiParallel.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <time.h>
 
 using namespace std;
 
@@ -31,11 +32,10 @@ int initializeMatrix(string origin){
 
 double newJacobi(int matrixSize){
     double suma, disp,var, aii;
+    int i,j;
     disp = 0;
-    int i=0;
-    int j = 0;
-    #pragma omp parallel for default(none) shared(xValues, xNewValues, matrix, matrixSize, disp) private(i, j, var, suma, aii)
-    for (int i = 0; i < matrixSize;++i){
+    #pragma omp parallel for shared(xValues, xNewValues, matrix, matrixSize, disp) private(i, j, var, suma, aii)
+    for (i = 0; i < matrixSize;++i){
         suma = 0;
         for (j = 0; j < matrixSize;++j){
             var = matrix[i][j];
@@ -56,9 +56,10 @@ bool jacobi(long long matrixSize, double tol, long long niter){
     int cont = 0;
 
     while (disp > tol && cont < niter){
-        for (int i = 0; i < matrixSize; ++i)xValues[i] = xNewValues[i];
+        for (int i = 0; i < matrixSize; ++i){
+            xValues[i] = xNewValues[i];
+        }
         disp = newJacobi(matrixSize);
-        cout << disp << endl;
         cont++;
     }
 
@@ -81,28 +82,34 @@ int main(){
     std::ios::sync_with_stdio(false);
     string matrixInputFile, outputFile;
     long long matrixSize, maxIterations;
+    clock_t start, end;
     double tolerance;
     bool success;
 
-    matrixInputFile = "matrix.txt";
-    outputFile = "sol.txt";
+    matrixInputFile = "./matrices/matrix1000.txt";
+    outputFile = "solution.txt";
 
     matrixSize = initializeMatrix(matrixInputFile);
 
     xValues.assign(matrixSize, 0);
     xNewValues.assign(matrixSize, 0);
 
-    cout << "how much tolerance? for example if you have n=10, tolerance should be 0.01 and maxIters=10000" << endl;
-    cin >> tolerance;
-    cout << "how many iterations? i.e: 100" << endl;
-    cin >> maxIterations;
+    //cout << "how much tolerance? for example if you have n=10, tolerance should be 0.01 and maxIters=10000" << endl;
+    //cin >> tolerance;
+    //cout << "how many iterations? i.e: 100" << endl;
+    //cin >> maxIterations;
 
+    tolerance=0.000001;
+    maxIterations=10000;   
+    start = clock(); 
     success = jacobi(matrixSize, tolerance, maxIterations);
+    end = clock(); 
+    printf("The time was: %.30g\n", (double)( (end - start) / 1000.0)); 
     
     if (success)
         writeSolution(outputFile);
     else
         cout << "could not reach the solutions in " << maxIterations << " iterations" << endl;
-        
+        writeSolution(outputFile);
     return 0;
 }
