ba9895971dcf0c6e77a0738113ffae3a01596e6e
Marcos Sierra <msierr37@eafit.edu.co>
2017-11-22 09:37:34 -0500

    .

--- aggregatedValue/python/errorPrinter.py
+++ aggregatedValue/python/errorPrinter.py
@@ -1,3 +1,4 @@
+#We have this code to print in a better way the error of every method
 from __future__ import print_function
 import sys
 def eprint(*args, **kwargs):
--- aggregatedValue/python/evaluator.py
+++ aggregatedValue/python/evaluator.py
@@ -1,4 +1,6 @@
+#Here we import de library math to do de evaluator
 import math
+#Here we pass the function and the evaluator return a result
 def f(num,s):
     funcs = vars(math)
     libres = dict(x=num)
